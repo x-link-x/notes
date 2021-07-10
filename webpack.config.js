49a3995092dd8906437997126dd1319d9ece086a
Link <xlinkx_@outlook.com>
2021-07-10 08:47:37 +0100

    set up file structure with npm and webpack

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const config = {
+    entry: `${__dirname}/src/app.js`,
+    output: {
+      path: `${__dirname}/public/js`,
+      filename: 'bundle.js'
+    },
+    mode: 'development'
+  }
+  module.exports = config;
