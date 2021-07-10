4b3efac67d016f2d394b252ca92c9288597d7293
Link <xlinkx_@outlook.com>
2021-07-10 08:56:00 +0100

    wrote note class

---
+++ src/models/note.js
@@ -0,0 +1,20 @@
+const Note = function (date, category, content, important=false) {
+    this.date = date;
+    this.category = category;
+    this.content = content;
+    this.important = important;
+}
+
+Note.prototype.getDate = function() {
+    return this.date;
+}
+
+Note.prototype.getContent = function() {
+    return this.content;
+}
+
+Note.prototype.setContent = function(content) {
+    this.content = content;
+}
+
+module.exports = Note;
